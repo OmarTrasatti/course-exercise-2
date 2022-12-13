2ed2c5f9b749d73a25cc147bdb40b74ada69826e
dave <dave@nddambros205320>
2022-12-13 12:49:48 +0100

    First commit

---
+++ src/main/java/it/cineca/springbootbeginner/SpringbootbeginnerApplication.java
@@ -0,0 +1,13 @@
+package it.cineca.springbootbeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbeginnerApplication.class, args);
+	}
+
+}
