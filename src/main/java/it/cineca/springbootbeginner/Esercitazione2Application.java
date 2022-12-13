e5e31408a1b2192adf2ddd658f6ef2536dc4045e
Emanuele Scapaticci <m.gattavecchia@cineca.it>
2022-12-13 12:39:50 +0100

    initial commit

---
+++ src/main/java/it/cineca/springbootbeginner/Esercitazione2Application.java
@@ -0,0 +1,13 @@
+package it.cineca.springbootbeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercitazione2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercitazione2Application.class, args);
+	}
+
+}
