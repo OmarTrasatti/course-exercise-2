787f4c4806f87b2c8f1ab041a921e7263c6503a9
SIMONE MIGNANI <s.mignani@cineca.it>
2022-12-19 15:15:10 +0100

    prova

--- src/main/java/it/cineca/springbootbeginner/app/SpringBeginnerDay2Application.java
+++ src/main/java/it/cineca/springbootbeginner/app/SpringBeginnerDay2Application.java
@@ -35,7 +35,7 @@ public class SpringBeginnerDay2Application {
 			repo.save(new Box(null, "NormalBox", "Author", null));
 			repo.save(new Box(null, "SquaredBox", "YetAnotherAuthor",null));
 			repo.save(new Box(null, "HexagonalBox", "YourAuthor", null));
-			repo.save(new Box(null, "PureBox", "ArthurKingOfBritons",null));
+			repo.save(new Box(null, "PureBox", "ArthurKingOfBritonss",null));
 		};
 	}
 }
